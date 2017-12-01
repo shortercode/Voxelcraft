fa978122b5c70b279a5a4d0ea4f277fc3a8b3761
Iain Shorter <iainshorter@gmail.com>
2017-12-01 15:28:27 +0000

    Fixed type resolution on server

--- src/server/index.js
+++ src/server/index.js
@@ -6,10 +6,44 @@ const util = require('util');
 const ROOT = "./client";
 const PORT = 80;
 
+function getMime (ext) {
+	switch (ext) {
+		case "html":
+			return "text/html";
+		case "js":
+			return "application/javascript";
+		default:
+			return 'text/plain';
+	}
+}
+
+function resolveSource (url) {
+	let source;
+	let mime;
+
+
+
+	if (url === "/") {
+		source = "/index.html";
+		mime = getMime("html");
+	}
+	else {
+		source = url;
+		const { ext } = path.parse(url);
+		mime = getMime(ext.slice(1));
+	}
+
+	source = ROOT + source;
+
+	return [ mime, source ];
+}
+
 const server = http.createServer((request, response) => {
-	const source = request.url === "/" ? "/index.html" : request.url;
-	const readStream = fs.createReadStream(ROOT + source);
+	const [ mime, source ] = resolveSource(request.url)
+	const readStream = fs.createReadStream(source);
+	console.log(mime, source);
 	readStream.on("open", () => {
+		response.writeHead(200, { 'Content-Type': mime });
 		readStream.pipe(response);
 	});
 	readStream.on("error", err => {
