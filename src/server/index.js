b5bfc7d9a94370abc800e4415051c836951821ee
Iain Shorter <iainshorter@gmail.com>
2017-12-01 11:21:32 +0000

    fixed server setup, added index.html

--- src/server/index.js
+++ src/server/index.js
@@ -3,16 +3,16 @@ const fs = require('fs');
 const path = require('path');
 const util = require('util');
 
-const ROOT = "./";
+const ROOT = "./client";
 const PORT = 80;
 
 const server = http.createServer((request, response) => {
-	const source = request.url;
-	const readStream = fs.createReadStream(source);
+	const source = request.url === "/" ? "/index.html" : request.url;
+	const readStream = fs.createReadStream(ROOT + source);
 	readStream.on("open", () => {
 		readStream.pipe(response);
 	});
-	readStream.on("error", () => {
+	readStream.on("error", err => {
 		response.statusCode = 400;
 		response.end(err.code);
 	})
