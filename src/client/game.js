d0d37020f5d8e20421c3ac1429827753b2272109
Iain Shorter <iainshorter@gmail.com>
2017-12-02 15:35:42 +0000

    Created basic game wrapper, which will be expanded

---
+++ src/client/game.js
@@ -0,0 +1,50 @@
+import { Renderer } from "./engine/Renderer.js";
+import { FirstPersonControls } from "./engine/FirstPersonControls.js";
+import { Dispatcher } from "./events/Dispatcher.js";
+
+class Game extends Dispatcher {
+	constructor () {
+		super();
+		this.renderer = new Renderer();
+		this.camera = this.renderer.createCamera(1, 45, 0.1, 1000);
+		this.scene = this.renderer.createScene();
+		this.shader = this.renderer.createDefaultShader();
+
+		this.controls = new FirstPersonControls(document, this);
+
+		this.renderer.setCamera(this.camera);
+		this.renderer.setScene(this.scene);
+		this.renderer.setShader(this.shader);
+
+		for (let x = -10; x < 10; x++) {
+			for (let y = -10; y < 10; y++) {
+				for (let z = -10; z < 10; z++) {
+					const cube = this.renderer.createEntity();
+					this.scene.add(cube);
+					cube.move(x, y, z);
+					cube.makeCube(0.1, 0.1, 0.1);
+					cube.setTexture("textures/bdc_stone01.png");
+				}
+			}
+		}
+
+		let time = performance.now();
+		const tick = () => {
+			requestAnimationFrame(tick);
+			const current = performance.now();
+			const dt = current - time;
+			time = current;
+			this.tick(dt);
+		};
+
+		tick();
+
+		document.body.appendChild(this.renderer.element);
+	}
+	tick (dt) {
+		this.emit("frame", dt);
+		this.renderer.render();
+	}
+}
+
+new Game();
