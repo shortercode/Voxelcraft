4ad95532b0c6d863e0c7e04c49573515d2f23233
shortercode <iainshorter@Iains-MacBook-Pro.local>
2017-12-02 18:37:04 +0000

    Adjusted some control bindings and added partially functional mouse lock

--- src/client/engine/FirstPersonControls.js
+++ src/client/engine/FirstPersonControls.js
@@ -4,91 +4,102 @@ import { Vector3 } from "../math/Vector3.js";
 
 export class FirstPersonControls {
 	constructor (doc, game, max, acc) {
+		this.canvas = game.renderer.element;
 		this.subscribers = new CompositeDisposable();
-		this.subscribers.add(Disposable.from(doc, "keydown", e => this.onKeyDown(e)));
-		this.subscribers.add(Disposable.from(doc, "keyup", e => this.onKeyDown(e)));
-		this.subscribers.add(Disposable.from(doc, "keydown", e => this.onKeyUp(e)));
-		this.subscribers.add(Disposable.from(doc, "mousemove", e => this.onMouseMove(e)));
+
+		this.subscribers.add(Disposable.from(document, "keydown", e => this.onKeyDown(e)));
+		this.subscribers.add(Disposable.from(document, "keyup", e => this.onKeyDown(e)));
+		this.subscribers.add(Disposable.from(document, "keydown", e => this.onKeyUp(e)));
+		this.subscribers.add(Disposable.from(document, "pointerlockchange", e => this.onPointerLockChange(e)));
+		this.subscribers.add(Disposable.from(document, "mousedown", e => this.onMouseDown(e)));
 		this.subscribers.add(game.on("frame", dt => this.onTick(dt)));
+
+		this.moveListener = null;
 		this.max = max;
 		this.rotation = new Vector3(0, 0, 0);
 		this.velocity = new Vector3(0, 0, 0);
 		this.camera = game.camera;
-		this.keys = {
-			w: false,
-			a: false,
-			s: false,
-			d: false,
-			space: false,
-			shift: false
+		this.keystate = {
+			"w": false,
+			"a": false,
+			"s": false,
+			"d": false,
+			" ": false,
+			"Shift": false
 		};
-
 	}
 	onTick (dt) {
-		const key = this.keys;
-
-		if (key.w != key.s) {
+		const keystate = this.keystate;
+		const delta = new Vector3();
+		if (keystate.w != keystate.s) {
 			// accelarate
-			if (key.w) {
+			delta.x = keystate.w ? 1 : -1;
+		}
 
-			} else {
+		if (keystate.a != keystate.d) {
+			delta.y = keystate.a ? 1 : -1;
+		}
 
-			}
+		if (keystate[" "] != keystate.Shift) {
+			delta.z = keystate.space ? 1 : -1;
+		}
+		const speed = 0.01 * dt;
+		this.camera.move(delta.multiply(speed));
+	}
+	onPointerLockChange (e) {
+		if (document.pointerLockElement === this.canvas) {
+			this.moveListener = Disposable.from(document, "mousemove", e => this.onMouseMove(e));
 		} else {
-			// decelarate
+			this.moveListener.dispose();
+			this.moveListener = null;
 		}
-
-		if (key.a != key.d) {
-			if (key.a) {
-
-			} else {
-
-			}
+	}
+	onMouseDown (e) {
+		if (document.pointerLockElement !== this.canvas) {
+			this.canvas.requestPointerLock();
 		}
-
-		if (key.space != key.shift) {
-			if (key.space) {
-
-			} else {
-
-			}
-		}
-
-		//this.camera.move(this.velocity);
-		this.camera.rotate(this.rotation.x, this.rotation.y, 0);
 	}
 	onKeyDown (e) {
 		e.preventDefault();
-		const delta = new Vector3();
 
 		switch (e.key) {
 			case "w":
-				delta.y = 1;
-				break;
 			case "a":
-				delta.x = -1;
-				break;
 			case "s":
-				delta.y = -1;
-				break;
 			case "d":
-				delta.x = 1;
-				break;
 			case " ":
-				delta.z = 1;
-				break;
 			case "Shift":
-				delta.z = -1;
+				this.keystate[e.key] = true;
 				break;
 		}
-		this.camera.move(delta.divide(4));
-		console.log(this.camera.position);
 	}
 	onKeyUp (e) {
+		e.preventDefault();
 
+		switch (e.key) {
+			case "w":
+			case "a":
+			case "s":
+			case "d":
+			case " ":
+			case "Shift":
+				this.keystate[e.key] = false;
+				break;
+		}
 	}
 	onMouseMove (e) {
-		this.rotation.y -= e.movementX / 200;
-		this.rotation.x += e.movementY / 200;
+		const sensitivity = 1 / 200;
+		const deltaX = e.movementX * sensitivity;
+		const deltaY = e.movementY * sensitivity;
+
+		const max = Math.PI * 0.5;
+		const rot = this.rotation;
+
+		const x = rot.x + deltaY;
+
+		rot.y += deltaX;
+		rot.x =  Math.abs(x) > max ? ( x < 0 ? -max : max ) :  x;
+
+		this.camera.rotate(rot.x, rot.y, 0);
 	}
 }
