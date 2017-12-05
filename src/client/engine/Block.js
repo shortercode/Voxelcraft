270cffc55eedf8b888a90ee33e728a16d62664b1
Iain Shorter <iainshorter@gmail.com>
2017-12-05 23:28:17 +0000

    Added block and chunk classes

---
+++ src/client/engine/Block.js
@@ -0,0 +1,72 @@
+import { Vector3 } from "../math/Vector3.js";
+import { getTextureCoords } from "./loadTexture.js";
+
+export class Block {
+	constructor (id, opaque) {
+		this.id = id;
+		this.opaque = opaque;
+		this.textures = {
+			top: getTextureCoords("default"),
+			bottom: getTextureCoords("default"),
+			left: getTextureCoords("default"),
+			right: getTextureCoords("default"),
+			front: getTextureCoords("default"),
+			back: getTextureCoords("default")
+		}
+	}
+	setAllTextures (src) {
+		const coords = getTextureCoords(src);
+		this.textures["top"] = coords;
+		this.textures["bottom"] = coords;
+		this.textures["left"] = coords;
+		this.textures["right"] = coords;
+		this.textures["front"] = coords;
+		this.textures["back"] = coords;
+	}
+	setTexture (side, src) {
+		this.textures[side] = getTextureCoords(src);
+	}
+	getTexture (side) {
+		return this.textures[side];
+	}
+	instance () {
+		return this;
+	}
+}
+
+Block.FRONT = [
+	new Vector3(0, 0, 1),
+	new Vector3(1, 0, 1),
+	new Vector3(1, 1, 1),
+	new Vector3(0, 1, 1),
+];
+Block.BACK = [
+	new Vector3(0, 0, 0),
+	new Vector3(0, 1, 0),
+	new Vector3(1, 1, 0),
+	new Vector3(1, 0, 0),
+];
+Block.TOP = [
+	new Vector3(0, 1, 0),
+	new Vector3(0, 1, 1),
+	new Vector3(1, 1, 1),
+	new Vector3(1, 1, 0),
+];
+Block.BOTTOM = [
+	new Vector3(0, 0, 0),
+	new Vector3(1, 0, 0),
+	new Vector3(1, 0, 1),
+	new Vector3(0, 0, 1),
+];
+Block.RIGHT = [
+	new Vector3(1, 0, 0),
+	new Vector3(1, 1, 0),
+	new Vector3(1, 1, 1),
+	new Vector3(1, 0, 1),
+];
+Block.LEFT = [
+	new Vector3(0, 0, 0),
+	new Vector3(0, 0, 1),
+	new Vector3(0, 1, 1),
+	new Vector3(0, 1, 0)
+];
---
+++ src/client/engine/Chunk.js
@@ -0,0 +1,137 @@
+import { Entity } from "./Entity.js";
+import { Block } from "./Block.js";
+
+export class Chunk {
+	constructor (gl, atlas, width, height, defaultBlock) {
+
+		this.width = width;
+		this.area = width ** 2;
+		this.height = height;
+		this.elements = [];
+
+		for (let i = 0; i < height; i++) {
+			const plane = [];
+			for (let ii = 0; ii < this.area; ii++) {
+				plane.push(defaultBlock.instance());
+			}
+			this.elements.push(plane);
+		}
+
+		this.entity = new Entity(gl);
+		this.entity.setTexture(atlas);
+
+		this.render();
+	}
+	render () {
+		console.time("render");
+		const getBlock = (plane, x, y) => {
+			if (!plane || y < 0 || x < 0 || x >= this.width || y >= this.width)
+				return null;
+			return plane[ y * this.width + x ];
+		}
+
+		const faces = [];
+
+		for (let i = 0; i < this.height; i++) {
+			const previousPlane = this.elements[i - 1];
+			const currentPlane = this.elements[i];
+			const nextPlane =  this.elements[i + 1];
+			for (let x = 0; x < this.width; x++) {
+				for (let y = 0; y < this.width; y++) {
+					const current = getBlock(currentPlane, x, y);
+					const top = getBlock(nextPlane, x, y);
+					const bottom = getBlock(previousPlane, x, y);
+					const front = getBlock(currentPlane, x, y + 1);
+					const back = getBlock(currentPlane, x, y - 1);
+					const left = getBlock(currentPlane, x - 1, y);
+					const right = getBlock(currentPlane, x + 1, y);
+
+					if (!current.opaque)
+						continue;
+
+					const positon = [ x, i, y ];
+					if (!top || !top.opaque) {
+						faces.push([ Block.TOP, positon, current.getTexture("top") ]);
+					}
+					if (!bottom || !bottom.opaque) {
+						faces.push([ Block.BOTTOM, positon, current.getTexture("bottom") ]);
+					}
+					if (!front || !front.opaque) {
+						faces.push([ Block.FRONT, positon, current.getTexture("front") ]);
+					}
+					if (!back || !back.opaque) {
+						faces.push([ Block.BACK, positon, current.getTexture("back") ]);
+					}
+					if (!left || !left.opaque) {
+						faces.push([ Block.LEFT, positon, current.getTexture("left") ]);
+					}
+					if (!right || !right.opaque) {
+						faces.push([ Block.RIGHT, positon, current.getTexture("right") ]);
+					}
+				}
+			}
+
+		}
+
+		const length = faces.length;
+		const count = length * 4;
+		const vertexArray = new Float32Array(count * 3);
+		const textureArray = new Float32Array(count * 2);
+		const indexArray = new Uint16Array(length * 6);
+
+		let i = 0;
+		let vi = 0;
+		let ti = 0;
+		let ii = 0;
+
+		for (const face of faces)
+		{
+			const verticies = face[0];
+			const position = face[1];
+			const texture = face[2];
+
+			vertexArray[vi++] = verticies[0].x + position[0];
+			vertexArray[vi++] = verticies[0].y + position[1];
+			vertexArray[vi++] = verticies[0].z + position[2];
+
+			vertexArray[vi++] = verticies[1].x + position[0];
+			vertexArray[vi++] = verticies[1].y + position[1];
+			vertexArray[vi++] = verticies[1].z + position[2];
+
+			vertexArray[vi++] = verticies[2].x + position[0];
+			vertexArray[vi++] = verticies[2].y + position[1];
+			vertexArray[vi++] = verticies[2].z + position[2];
+
+			vertexArray[vi++] = verticies[3].x + position[0];
+			vertexArray[vi++] = verticies[3].y + position[1];
+			vertexArray[vi++] = verticies[3].z + position[2];
+
+			textureArray[ti++] = texture[0];
+			textureArray[ti++] = texture[1];
+
+			textureArray[ti++] = texture[2];
+			textureArray[ti++] = texture[3];
+
+			textureArray[ti++] = texture[4];
+			textureArray[ti++] = texture[5];
+
+			textureArray[ti++] = texture[6];
+			textureArray[ti++] = texture[7];
+
+			indexArray[ii++] = i;
+			indexArray[ii++] = i + 1;
+			indexArray[ii++] = i + 2;
+			indexArray[ii++] = i;
+			indexArray[ii++] = i + 2;
+			indexArray[ii++] = i + 3;
+
+			i += 4;
+		}
+
+		this.entity.setVertexBuffer(vertexArray);
+		this.entity.setTextureBuffer(textureArray);
+		this.entity.setIndexBuffer(indexArray);
+
+		console.timeEnd	("render");
+	}
+}
