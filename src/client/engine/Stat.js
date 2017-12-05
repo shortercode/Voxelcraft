a4ec2d2f855bea89dde5ea3e044bd7918acd5a90
Iain Shorter <iainshorter@gmail.com>
2017-12-05 23:27:03 +0000

    Slight adjustments to stat counter

--- src/client/engine/Stat.js
+++ src/client/engine/Stat.js
@@ -2,14 +2,14 @@ export class Stat {
 	constructor (game) {
 		this.element = document.createElement("div");
 		document.body.appendChild(this.element);
-		this.element.style.cssText = "background: white; color: black; width: 50px; height: 20px; font-height: 20px; position: absolute; left: 0; top: 0;";
+		this.element.style.cssText = "background: white; color: black; width: 50px; font-family: monospace; height: 20px; font-height: 20px; position: absolute; left: 0; top: 0;";
 
 		let average = null;
 		game.on("frame", dt => {
 			if (!average) {
 				average = dt;
 			} else {
-				average += (dt - average) * 0.5;
+				average += (dt - average) * 0.3;
 			}
 			this.element.textContent = `${Math.round(1000 / average)}fps`;
 		});
