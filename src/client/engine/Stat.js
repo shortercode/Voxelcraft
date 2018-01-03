7ada70f0c3900af3cc472537f15a507136555098
shortercode <iainshorter@iains-mbp.home>
2018-01-03 18:22:29 +0000

    Added new "oak_leaf" block

--- src/client/engine/Stat.js
+++ src/client/engine/Stat.js
@@ -2,7 +2,7 @@ export class Stat {
 	constructor (game) {
 		this.element = document.createElement("div");
 		document.body.appendChild(this.element);
-		this.element.style.cssText = "background: white; color: black; width: 50px; font-family: monospace; height: 20px; font-height: 20px; position: absolute; left: 0; top: 0;";
+		this.element.style.cssText = "background: white; z-index: 2; color: black; width: 50px; font-family: monospace; height: 20px; font-height: 20px; position: absolute; left: 0; top: 0;";
 
 		let average = null;
 		game.on("frame", dt => {
