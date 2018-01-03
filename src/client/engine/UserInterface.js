a0cbbf62c6de3d0c4cb71acf8c3f177da76c134c
shortercode <iainshorter@iains-mbp.home>
2018-01-03 18:24:42 +0000

    Started on UserInterface class

---
+++ src/client/engine/UserInterface.js
@@ -0,0 +1,47 @@
+class UserInterface {
+	constructor () {
+		this.element = document.createElement('canvas');
+		this.context = this.element.getContext('2d');
+		this.element.style.zIndex = 1;
+		this.pixelRatio = devicePixelRatio;
+		this.width = 0;
+		this.height = 0;
+	}
+	updateSize () {
+		const w = innerWidth;
+		const h = innerHeight;
+
+		if (this.width != w || this.height != h)
+			this.setSize(w, h);
+	}
+	setSize (width, height) {
+		const correctedWidth = width * this.pixelRatio;
+		const correctedHeight = height * this.pixelRatio;
+		const ratio = width / height;
+		this.width = width;
+		this.height = height;
+		this.element.style.width = `${width}px`;
+		this.element.style.height = `${height}px`;
+		this.element.width = correctedWidth;
+		this.element.height = correctedHeight;
+	}
+	render () {
+
+	}
+}
+
+class Element {
+	constructor () {
+		this.position = { x: 0, y: 0 };
+		this.size = { x: 0, y: 0 };
+	}
+	hitTest ({ x, y }) {
+		const P = this.position;
+		const S = this.size;
+
+		return x > P.x &&
+			y > P.y &&
+			x < P.x + S.x &&
+			y < P.y + S.y;
+	}
+}
