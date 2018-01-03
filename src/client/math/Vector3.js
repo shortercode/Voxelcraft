7f9f2527bfef05ca7089acb18fc8b797f0b81b62
shortercode <iainshorter@iains-mbp.home>
2018-01-03 18:21:36 +0000

    Added extra basic direction constants to Vector3

--- src/client/math/Vector3.js
+++ src/client/math/Vector3.js
@@ -118,8 +118,13 @@ export class Vector3 {
 		return this.x * x + this.y * y + this.z * z;
 	}
 	equals ({x, y, z}) {
-		return this.x === x && this.y === y && this.z === z;  
+		return this.x === x && this.y === y && this.z === z;
 	}
 }
 
 Vector3.UP = new Vector3(0, 1, 0);
+Vector3.DOWN = new Vector3(0, -1, 0);
+Vector3.LEFT = new Vector3(-1, 0, 0);
+Vector3.RIGHT = new Vector3(1, 0, 0);
+Vector3.FORWARD = new Vector3(0, 0, 1);
+Vector3.BACKWARD = new Vector3(0, 0, -1);
