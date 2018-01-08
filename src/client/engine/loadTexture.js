d8caa25681262cd8a27bd179066c1d04180acb47
Iain Shorter <iainshorter@gmail.com>
2018-01-08 21:44:54 +0000

    Removed old texture atlas method from loadTexture

--- src/client/engine/loadTexture.js
+++ src/client/engine/loadTexture.js
@@ -39,73 +39,6 @@ async function blendTexture (img, texSize, r, g, b) {
 
 	return await createImageBitmap(canvas);
 }
-//
-// export async function createAtlas (gl, images, texSize) {
-//
-// 	images = Array.from(images);
-// 	const bitmaps = await Promise.all(images.map(src => fetchImage(`textures/${src}.png`)));
-// 	const n = 2 ** Math.ceil(Math.log2(Math.ceil(Math.sqrt(images.length))));
-// 	const size = texSize * n;
-// 	const canvas = document.createElement('canvas');
-// 	const ctx = canvas.getContext('2d');
-// 	const coords = [];
-// 	const ln = 1 / n;
-//
-// 	canvas.width = size;
-// 	canvas.height = size;
-//
-// 	ctx.fillStyle = "rgba(0, 0, 0, 0)";
-// 	ctx.fillRect(0, 0, size, size);
-//
-// 	for (let x = 0, i = 0; x < n; x++) {
-// 		for (let y = 0; y < n; y++, i++) {
-// 			let image = bitmaps[i];
-// 			const src = images[i];
-// 			if (!image)
-// 				break;
-//
-// 			if (src == "grass_top") {
-// 				image = await blendTexture(image, texSize, 0x87, 0xba, 0x45);
-// 				//blend(ctx, texSize, x, y, "#87ba45")
-// 			}
-// 			else if (src == "leaves_oak")
-// 			{
-// 				image = await blendTexture(image, texSize, 0x87, 0xba, 0x45);
-// 				//blend(ctx, texSize, x, y, "#87ba45")
-// 			}
-//
-// 			ctx.drawImage(image, x * texSize, y * texSize);
-//
-// 			const x1 = x * ln;
-// 			const y1 = y * ln;
-// 			const x2 = (x + 1) * ln;
-// 			const y2 = (y + 1) * ln;
-//
-// 			textureMap.set(src, [
-// 				x1, y1,
-// 				x2, y1,
-// 				x2, y2,
-// 				x1, y2
-// 			]);
-// 		}
-// 	}
-//
-// 	//document.body.appendChild(canvas);
-//
-// 	const atlasBitmap = await createImageBitmap(canvas);
-//
-// 	const texture = gl.createTexture();
-// 	const TEX_2D = gl.TEXTURE_2D;
-//
-// 	gl.bindTexture(TEX_2D, texture);
-// 	gl.texImage2D(TEX_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, atlasBitmap);
-// 	gl.texParameteri(TEX_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
-// 	gl.texParameteri(TEX_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST_MIPMAP_LINEAR);
-// 	gl.generateMipmap(TEX_2D);
-// 	gl.bindTexture(TEX_2D, null);
-//
-// 	return texture;
-// }
 
 export async function create3DTexture (gl, images, texSize) {
 
@@ -140,7 +73,7 @@ export async function create3DTexture (gl, images, texSize) {
 	}
 
 	gl.texParameteri(TEX_ARRAY, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
-	gl.texParameteri(TEX_ARRAY, gl.TEXTURE_MIN_FILTER, gl.NEAREST_MIPMAP_LINEAR);
+	gl.texParameteri(TEX_ARRAY, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
 	gl.generateMipmap(TEX_ARRAY);
 	gl.bindTexture(TEX_ARRAY, null);
 
